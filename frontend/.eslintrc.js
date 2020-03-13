4a2185cfcf08f478e7094fc1c14e8eabcadec75f
zrma <bulbitain@gmail.com>
2020-03-14 01:16:20 +0900

    frontend: add vue.config and update eslintrc

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,19 +1,24 @@
 module.exports = {
   root: true,
+
   env: {
     node: true,
   },
+
   extends: [
     'plugin:vue/essential',
     '@vue/airbnb',
   ],
+
   parserOptions: {
     parser: 'babel-eslint',
   },
+
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
+
   overrides: [
     {
       files: [
@@ -25,4 +30,9 @@ module.exports = {
       },
     },
   ],
+
+  'extends': [
+    'plugin:vue/strongly-recommended',
+    '@vue/airbnb'
+  ]
 };
