eac4697dbf0b79b2e6740385e59edbab586334f5
zrma <bulbitain@gmail.com>
2020-03-17 00:19:16 +0900

    frontend: implement comment api get

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,15 +1,14 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import comments from './modules/comments';
 
 Vue.use(Vuex);
 
+const debug = process.env.NODE_ENV !== 'production';
+
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+  strict: debug,
   modules: {
+    comments,
   },
 });
