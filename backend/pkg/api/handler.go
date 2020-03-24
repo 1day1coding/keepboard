40353aa7c8be2150b80fc7944a254b58473c5c25
zrma <bulbitain@gmail.com>
2020-03-24 21:40:48 +0900

    backend: implement comment api post

--- backend/pkg/api/handler.go
+++ backend/pkg/api/handler.go
@@ -7,21 +7,39 @@ import (
 )
 
 type Handler struct {
-	getComment func() ([]string, error)
+	getComments func() ([]string, error)
+	addComment  func(string) error
 }
 
-func New(getComment func() ([]string, error)) *Handler {
-	return &Handler{getComment: getComment}
+func New(getComments func() ([]string, error), addComment func(string) error) *Handler {
+	return &Handler{
+		getComments: getComments,
+		addComment:  addComment,
+	}
 }
 
 func (h *Handler) HealthCheck(c echo.Context) error {
 	return c.NoContent(http.StatusOK)
 }
 
-func (h *Handler) GetComments(c echo.Context) error {
-	res, err := h.getComment()
+func (h *Handler) GetComment(c echo.Context) error {
+	res, err := h.getComments()
 	if err != nil {
 		return echo.NewHTTPError(http.StatusInternalServerError)
 	}
 	return c.JSON(http.StatusOK, res)
+}
+
+func (h *Handler) AddComment(c echo.Context) error {
+	type aux struct {
+		Comment string `json:"comment"`
+	}
+	var input aux
+	if err := c.Bind(&input); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	if err := h.addComment(input.Comment); err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+	return c.NoContent(http.StatusOK)
 }
--- backend/pkg/api/handler_test.go
+++ backend/pkg/api/handler_test.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"bytes"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
@@ -12,10 +13,14 @@ import (
 
 func TestHandler(t *testing.T) {
 	expected := []string{"a", "b", "c"}
+	var actualComment string
 
 	e := echo.New()
 	h := New(func() ([]string, error) {
 		return expected, nil
+	}, func(comment string) error {
+		actualComment = comment
+		return nil
 	})
 
 	t.Run("/ 헬스체크 테스트", func(t *testing.T) {
@@ -35,12 +40,32 @@ func TestHandler(t *testing.T) {
 		rec := httptest.NewRecorder()
 		c := e.NewContext(req, rec)
 
-		assert.NoError(t, h.GetComments(c))
+		assert.NoError(t, h.GetComment(c))
 		assert.Equal(t, http.StatusOK, rec.Code)
 
 		var actual []string
 		err := json.Unmarshal(rec.Body.Bytes(), &actual)
 		assert.NoError(t, err)
 		assert.Equal(t, actual, expected)
+	})
+
+	t.Run("/comment 입력 테스트", func(t *testing.T) {
+		const expectedComment = "msg1234"
+		type aux struct {
+			Comment string `json:"comment"`
+		}
+		input := aux{expectedComment}
+		data, err := json.Marshal(input)
+		assert.NoError(t, err)
+
+		req := httptest.NewRequest(http.MethodPost, "/api/comment", bytes.NewReader(data))
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
+
+		assert.NoError(t, h.AddComment(c))
+		assert.Equal(t, http.StatusOK, rec.Code)
+
+		assert.Equal(t, expectedComment, actualComment)
 	})
 }
